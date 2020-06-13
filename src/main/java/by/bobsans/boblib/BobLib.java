7db1e5366d65eedddaa3a4f13b109ba9cd77eaa7
Bobsans <mr.bobsans@gmail.com>
2020-06-14 00:36:27 +0300

    drop networking support

--- src/main/java/by/bobsans/boblib/BobLib.java
+++ src/main/java/by/bobsans/boblib/BobLib.java
@@ -1,13 +1,6 @@
 package by.bobsans.boblib;
 
-import by.bobsans.boblib.network.NetworkingManager;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Reference.MODID)
-public class BobLib {
-    public BobLib() {
-        MinecraftForge.EVENT_BUS.register(this);
-        NetworkingManager.init();
-    }
-}
+public class BobLib {}
