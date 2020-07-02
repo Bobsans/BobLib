790ca706497281f8b79b1d7608b7036ea3287159
Bobsans <mr.bobsans@gmail.com>
2020-07-02 18:32:23 +0300

    add double value input

---
+++ src/main/java/by/bobsans/boblib/gui/widgets/value/OptionsEntryValueDouble.java
@@ -0,0 +1,21 @@
+package by.bobsans.boblib.gui.widgets.value;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.function.Consumer;
+
+public class OptionsEntryValueDouble extends OptionsEntryValueInput<Double> {
+    private OptionsEntryValueDouble(String optionName, Double value, Consumer<Double> save) {
+        super(optionName, value, save);
+
+        textField.setValidator((s) -> s.matches("^[0-9.]*$"));
+    }
+
+    public OptionsEntryValueDouble(String langKeyPrefix, ForgeConfigSpec.DoubleValue spec) {
+        this(getLangKey(spec, langKeyPrefix), spec.get(), spec::set);
+    }
+
+    public void setValue(String text) {
+        value = Double.valueOf(text);
+    }
+}
